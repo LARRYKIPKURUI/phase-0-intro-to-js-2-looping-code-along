72f0bef66994b3a4335f07f50e038fb1f5280405
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-03 00:24:54 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let messages = []; 
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages; 
+}
+const namesArray = ["Ali", "Samip", "Charlie"];
+const eventName = "birthday";
+console.log(writeCards(namesArray, eventName));
+
+
+function countDown (countNum) {
+    while (countNum >= 0){
+        console.log(countNum);
+        countNum--;
+    }
+}
+
+countDown(10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
